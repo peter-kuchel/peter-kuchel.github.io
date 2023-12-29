c77ae918e4661530ffd4611c473a1f007474a52d
Peter Kuchel <petermichaelkuchel@gmail.com>
2023-12-29 07:04:28 +0000

    hiding port and host to seperate file

--- minserver.js
+++ minserver.js
@@ -2,16 +2,12 @@
 // minimal http server just for testing the site out
 
 const http = require('http');
-const netconfig = require('./netconfig');
-
-const PORT = netconfig.PORT;
-const HOST = netconfig.HOST;
+const { PORT, HOST } = require('./netconfig');
 
 const server = http.createServer((req, res) => {
     // find html file to run -- assume index.html
 
 
-    // console.log(req.headers);
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     res.write('<h>Hello Cho Cho\n<\h>');
