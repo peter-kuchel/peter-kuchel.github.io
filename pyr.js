3bc74b640be2fc1ed00a3ab4b60d041ab41ffbf5
Peter Kuchel <petermichaelkuchel@gmail.com>
2025-01-26 18:55:06 -0700

    small animation added, rotating a square, eventually will be a cube

--- pyr.js
+++ pyr.js
@@ -1,15 +1,45 @@
 
 (() => {
+	
+	let canvas = document.getElementById("c")
+	let tmr = undefined
+	let origin = [325, 225]
+	let a = [-50, -50], b = [50, -50], c = [50, 50], d = [-50, 50]
+	let ps = [a, b, c, d, a]
+	let ps_p = [[0,0],[0,0],[0,0],[0,0],[0,0]]
+	let t = 0
 
-let c = document.getElementById("c")
-let ctx = c.getContext("2d")
+	const rotate = () => {
+		
+		let ctx = canvas.getContext("2d")
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-ctx.fillStyle = "white"
-ctx.fillRect(0, 0, c.width, c.height)
+		t += 0.09
+		let st = Math.sin(t) 
+		let ct = Math.cos(t)
 
-ctx.moveTo(0,0)
-ctx.lineTo(c.width - 50, c.height - 50)
+		let i = null
+		for (i = 0; i < ps.length; i++){
+			ps_p[i][0] = (ps[i][0] * ct ) - (ps[i][1] * st)
+			ps_p[i][1] = (ps[i][0] * st ) + (ps[i][1] * ct) 
+		}
+		
+		ctx.strokeStyle = '#07ef7b'
+		for (i = 1; i < ps.length; i++){
+			ctx.beginPath()
+			ctx.moveTo(origin[0] + ps_p[i-1][0], origin[1] + ps_p[i-1][1])
+			ctx.lineTo(origin[0] + ps_p[i][0], origin[1] + ps_p[i][1])
+			ctx.stroke()
+			ctx.closePath()
+		}
 
-ctx.stroke()
+	}
+	tmr = setInterval(rotate, 45)
+	//window.requestAnimationFrame(rotate)
+})()
 
-})();
+
+/* 
+
+*/
+
