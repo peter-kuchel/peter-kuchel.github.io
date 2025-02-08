3db2563c45f4f1f3e8f8489f3ac6751d3f0093c1
Peter Kuchel <petermichaelkuchel@gmail.com>
2025-02-07 22:37:56 -0700

    changes made to point coordinates and rotation vars + equations

--- pyr.js
+++ pyr.js
@@ -4,18 +4,18 @@
 	let canvas = document.getElementById("c")
 	let tmr = undefined
 
-	const xo = 325, yo = 225, zo = 0, so = 0, Z1 = 25, Z2 = 25
+	const xo = 325, yo = 225, zo = 0, so = 0, Z1 = 35, Z2 = 20
 	let origin = [(xo + (so / 2) + (so / 2)), (yo + (so / 2) + (so / 2))]
 
 	let a = [-50, -50, 0], b = [50, -50, 0], c = [50, 50, 0], d = [-50, 50, 0],
-	    e = [0, -100, -50], f = [100, -100, -50], g = [100, 0, -50], h = [0,0, -50]
+	    e = [-50, -50, -100], f = [50, -50, -100], g = [50, 50, -100], h = [-50, 50, -100]
 
 	let ps_f1 = [a, b, c, d, a]
 	let ps_f2 = [e, f, g, h, e]
 	let ps_p1 = [[0,0],[0,0],[0,0],[0,0],[0,0]]
 	let ps_p2 = [[0,0],[0,0],[0,0],[0,0],[0,0]]
 
-	let t = 0, p = 0, om = 0
+	let _a = 0, _b = 0, _g = 0
 	const dis = 7	
 
 	const rotate = () => {
@@ -23,12 +23,12 @@
 		let ctx = canvas.getContext("2d")
 		ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-		t += -0.01
-		p += 0.02
-		om += -0.07
+		_a += 0.0
+		_b += -0.0
+		_g += 0.02
 
-		let st = Math.sin(t), sp = Math.sin(p), som = Math.sin(om) 
-		let ct = Math.cos(t), cp = Math.cos(p), com = Math.cos(om)
+		let sa = Math.sin(_a), sb = Math.sin(_b), sg = Math.sin(_g), 
+		    ca = Math.cos(_a), cb = Math.cos(_b), cg = Math.cos(_g)
 		
 		const draw_line = (x0, y0, x1, y1, ctx) => {
 
@@ -51,18 +51,27 @@
 			-x*sin(b) + y*sin(a)*cos(b) + z*cos(a)*cos(b)
 		*/
 
-		let cpcom = cp * com, stsp = st * sp, spct = sp * ct
-		let _x = 0, _y = 0, _z = 0
+		//let cpcom = cp * com, stsp = st * sp, spct = sp * ct
+		let cbcg = cb * cg, sasb = sa * sb, sbca = sb * ca
+		let _x = 0, _y = 0, _z = 0, xp = 0, yp = 0, zp = 0
 		for (i = 0; i < ps_f1.length; i++){
 			
 			_x = ps_f1[i][0], _y = ps_f1[i][1], _z = ps_f1[i][2] 
-			ps_p1[i][0] = (_x * cpcom) + (_y * stsp * com) - (_y * som * ct) + (_z * st * som) + (_z * spct * com) 
-			ps_p1[i][1] = (_x * cpcom) + (_y * stsp * som) - (_y * com * ct) + (_z * st * com) + (_z * spct * som)   
+			xp = (_x * cbcg) + (_y * sasb * cg) - (_y * sg * ca) + (_z * sa * sg) + (_z * sbca * cg)
+			yp = (_x * cbcg) + (_y * sasb * sg) + (_y * ca * cg) - (_z * sa * cg) + (_z * sbca * sg)
+			//zp = (-_x * sp) + (_y * st * cp) + (_z * ct * cp)
+
+			ps_p1[i][0] = (Z1 * xp) / ( Z2) 
+			ps_p1[i][1] = (Z1 * yp) / ( Z2) 
 
 			_x = ps_f2[i][0], _y = ps_f2[i][1], _z = ps_f2[i][2] 
-			ps_p2[i][0] = (_x * cpcom) + (_y * stsp * com) - (_y * som * ct) + (_z * st * som) + (_z * spct * com) 
-			ps_p2[i][1] = (_x * cpcom) + (_y * stsp * som) - (_y * com * ct) + (_z * st * com) + (_z * spct * som)   
+			xp = (_x * cbcg) + (_y * sasb * cg) - (_y * sg * ca) + (_z * sa * sg) + (_z * sbca * cg)
+			yp = (_x * cbcg) + (_y * sasb * sg) + (_y * ca * cg) - (_z * sa * cg) + (_z * sbca * sg)
+			//zp = (-_x * sp) + (_y * st * cp) + (_z * ct * cp)
 
+
+			ps_p2[i][0] = (Z1 * xp) / ( Z2) 
+			ps_p2[i][1] = (Z1 * yp) / ( Z2) 
 		}
 		
 		ctx.strokeStyle = '#07ef7b'
